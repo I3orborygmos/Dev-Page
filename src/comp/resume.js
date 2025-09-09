c687c0e75323a90a439ef9cf25fab15cf8ffd6e8
I3orborygmos <connor.mcdonald24@gmail.com>
2025-09-08 23:00:32 -0700

    28th

--- src/comp/resume.js
+++ src/comp/resume.js
@@ -1,10 +1,20 @@
 export default function Resume() {
-    
+    const isMobile = window.innerWidth <= 600;
+
     return (
-        <div className='resume'>
+        <div style={{ width: '100%', height: '100vh' }}>
             <p style={{ textAlign: 'center', marginTop: '1em' }}>
-                If the PDF does not display, <a href="/Connor McDonald_Frontend_Website_Developer.pdf" target="_blank" rel="noopener noreferrer">click here to view or download the resume.</a>
+                If the PDF does not display, <a href="/Connor_McDonald_Frontend_Website_Developer.pdf" target="_blank" rel="noopener noreferrer">click here to view or download the resume.</a>
             </p>
+            {!isMobile && (
+                <embed
+                    src="/Connor_McDonald_Frontend_Website_Developer.pdf"
+                    type="application/pdf"
+                    width="100%"
+                    height="100%"
+                    style={{ minHeight: 500 }}
+                />
+            )}
         </div>
     );
 }
